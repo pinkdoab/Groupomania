73075e0630733608032545b1ca45fc10ac0d5aa2
pinkdoab <divers@doab.fr>
2020-10-26 16:55:05 +0100

    structure simple lecture mysql par frontend

--- backend/server/server.js
+++ backend/server/server.js
@@ -5,6 +5,13 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
 app.use('/api/animal', apiRouter);
 
 app.listen(process.env.PORT || '3000', () => {
