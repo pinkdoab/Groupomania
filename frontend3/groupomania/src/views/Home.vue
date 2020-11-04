a9d3509e333daf278021cbf5da2b53640c5dc96a
pinkdoab <divers@doab.fr>
2020-11-04 10:18:53 +0100

    réorganisation frontend, enleve dossier inutile

---
+++ frontend3/groupomania/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <ListPosts/>
+  </div>
+</template>
+
+
+<script>
+import ListPosts from '@/components/ListPosts.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    ListPosts
+  }
+}
+</script>
